166367820ac5fb1d0036b1e55dc592575be9ff21
Administrator <2533064395@qq.com>
2019-04-23 16:55:15 +0800

    2019/4/23

--- src/main/java/com/springmybatis/springbootmybatis/util/SexEnmu.java
+++ src/main/java/com/springmybatis/springbootmybatis/util/SexEnmu.java
@@ -3,14 +3,31 @@ package com.springmybatis.springbootmybatis.util;
 /**
  * @author amc2533064395@163.com
  * @date 2019/4/20 13:21
+ * 性别枚举
  */
 public enum  SexEnmu {
-    men("男",1), women("女",2);
-    private String sexs;
-    private  int index;
+    men(1,"男"),women(2,"女");
+    /**
+     * 识别号
+     */
+    private int index;
+    /**
+     * 性别
+     */
+    private  String sexs;
 
-    SexEnmu(String sexs, int index) {
-        this.sexs = sexs;
+    SexEnmu(int index, String sexs) {
         this.index = index;
+        this.sexs = sexs;
+    }
+
+    public  static  SexEnmu getSexEnmu(int code){
+        SexEnmu [] enmus=SexEnmu.values();
+        for( SexEnmu item:enmus){
+            if(item.index==code){
+                return  item;
+            }
+        }
+        return  null;
     }
 }
